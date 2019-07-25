c1e6db7266bd61958a8743b797ed5b8b66222c99
fieldr <fieldr92@gmail.com>
2019-07-25 15:27:27 +0100

    correct jsx

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -11,35 +11,27 @@ const HomePage = () => {
             <span className="subtitle">Shop now</span>
           </div>
         </div>
-      </div>
-      <div className="directory-menu">
         <div className="menu-item">
           <div className="content">
             <h1 className="title">JACKETS</h1>
             <span className="subtitle">Shop now</span>
           </div>
         </div>
-      </div>
-      <div className="directory-menu">
         <div className="menu-item">
           <div className="content">
             <h1 className="title">TRAINERS</h1>
             <span className="subtitle">Shop now</span>
           </div>
         </div>
-      </div>
-      <div className="directory-menu">
         <div className="menu-item">
           <div className="content">
             <h1 className="title">WOMENS</h1>
             <span className="subtitle">Shop now</span>
           </div>
         </div>
-      </div>
-      <div className="directory-menu">
         <div className="menu-item">
           <div className="content">
-            <h1 className="title">SHOES</h1>
+            <h1 className="title">MENS</h1>
             <span className="subtitle">Shop now</span>
           </div>
         </div>
