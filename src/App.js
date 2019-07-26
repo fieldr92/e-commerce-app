0f24890abba801f392a8b462780dbebfabd5f9fb
fieldr <fieldr92@gmail.com>
2019-07-26 16:02:40 +0100

    update menu items to navigate with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
-function App() {
+const HatsPage = props => {
+  console.log(props);
+  return <h1>Hats Page</h1>;
+};
+
+const App = () => {
   return (
     <div>
-      <HomePage />
+      <Route exact path="/" component={HomePage} />
+      <Route path="/hats" component={HatsPage} />
     </div>
   );
-}
+};
 
 export default App;
