322300758f3b91bb4c491c1f672add9292757d3b
fieldr <fieldr92@gmail.com>
2019-07-29 10:46:51 +0100

    add sign-in page, custom button and input-form components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInRegister from './pages/signin-register/signin-register.component';
 import Header from './components/header/header.component';
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInRegister} />
       </Switch>
     </div>
   );
---
+++ src/pages/signin-register/signin-register.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SignIn from '../../components/sign-in/sign-in.component';
+
+import './signin-register.styles.scss';
+
+const SignInRegister = () => (
+  <div className="sign-in-and-sign-up">
+    <SignIn />
+  </div>
+);
+
+export default SignInRegister;
